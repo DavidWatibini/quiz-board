91cc5354ee88cbbc2b187726c9625b259ffd4c60
Watinguyaz <watimakhanu@gmail.com>
2018-12-11 11:45:53 +0300

    it will update everything

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 //Busisness logic//
 
-function calcScore(){
-    var myScore = 0;
-    $(".calc:checked").each(function(){
-        myScore+=parseInt($(this).val(),10);
-    });
-    $("input[name=sum]").val(myScore)
+function calcScore() {
+  var myScore = 0;
+  $(".calc:checked").each(function() {
+    myScore += parseInt($(this).val(), 10);
+  });
+  $("input[name=sum]").val(myScore)
 }
-$().ready(function(){
-    $(".calc").change(function(){
-        calcScore()
-    });
+$().ready(function() {
+  $(".calc").change(function() {
+    calcScore()
+  });
 });
 
 function reset() {
@@ -25,14 +25,14 @@ $(document).ready(function() {
   });
 });
 
-$(document).ready(function(){
-    $("#show").click(function(){
-        $(".container").animate({
-            left: '40px',
-            opacity:0.9
+$(document).ready(function() {
+  $("#show").click(function() {
+    $(".container").animate({
+      left: '40px',
+      opacity: 0.9
 
-        });
     });
+  });
 });
 
 $(document).ready(function() {
@@ -45,19 +45,7 @@ $(document).ready(function() {
 $(document).ready(function() {
   $("#myScore").click(function() {
     $(".questions").hide();
-      $("#showTest").hide();
+    $("#showTest").hide();
     $(".myScore").show();
   });
 });
-
-//function result() {
-  //var sides = [];
-  //sides.push(document.getElementById("full-names").value);
-  //sides.push(document.getElementById("email-address").value);
-  //var [a, b] = sides;
-  //var display = document.getElementById("display");
-
-  //if (a.length === 0 || b.length === 0 || c.length === 0) {
-    //alert("Please fill in all fields");
-    //confirm("Click on help for more information")
-  //}
